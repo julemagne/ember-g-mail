2829b01fa0a967a4989b2495610532eef4a6deb2
Julie David <julie.angela.david@gmail.com>
2016-08-16 11:18:54 -0400

    add trashBulk action to controllers/folder/index and checkboxes in templates/folder/index so that multiple email models can be marked as checked and have trashedDate added, so they will be removed from the inbox at the same time.

--- app/controllers/folder/index.js
+++ app/controllers/folder/index.js
@@ -1,9 +1,23 @@
 import Ember from 'ember';
 
+const { get, set } = Ember;
+
 export default Ember.Controller.extend({
 	actions: {
 		showEmail(email) {
 			this.transitionToRoute('folder.mail', email);
-		}
+		},
+    trashBulk() {
+    	const results = get(this, 'model').filter((item) => {
+    		return get(item, 'checked');
+    	});
+    	// ES6 way:
+    	// const results = get(this, 'model').filter(item => get(item, 'checked'));
+    	results.forEach((result) => {
+    		set(result, 'trashedDate', new Date());
+    		set(result, 'checked', false);
+    	});
+    	this.transitionToRoute('application');
+    }
 	}
 });
